fd3b967fd66bcbb1e47fc82abec991f2ed340666
ola <olasvoice@gmail.com>
2019-08-13 15:45:50 +0100

    uppercased App in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  entry: './src/app.js',
+  entry: './src/App.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve('dist'),
